b79b5855fae3f41a64a2b8ab60a784a55c43d321
Poligonais <cgpg@mpor.gov.br>
2026-01-29 10:47:44 -0300

    Add files via upload

--- styles/AnexoIIIBotafora_1_style.js
+++ styles/AnexoIIIBotafora_1_style.js
@@ -6,22 +6,22 @@ var style_AnexoIIIBotafora_1 = function(feature, resolution){
         feature: feature,
         variables: {}
     };
-    var value = ""
-    var labelText = "";
-    size = 0;
+    
+    var labelText = ""; 
+    var value = feature.get("");
     var labelFont = "10px, sans-serif";
     var labelFill = "#000000";
     var bufferColor = "";
     var bufferWidth = 0;
     var textAlign = "left";
-    var offsetX = 8;
-    var offsetY = 3;
+    var offsetX = 0;
+    var offsetY = 0;
     var placement = 'point';
     if ("" !== null) {
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(75,255,252,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 2.508}),fill: new ol.style.Fill({color: 'rgba(75,255,252,0.24313725490196078)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(75,255,252,1.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 3.268}),fill: new ol.style.Fill({color: 'rgba(75,255,252,0.2901960784313726)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
